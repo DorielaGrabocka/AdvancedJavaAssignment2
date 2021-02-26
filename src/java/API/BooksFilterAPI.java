c285ea1d28c32f40dfe6660c0bfbcaff97c6a515
DorielaGrabocka <dorielagrabocka@unyt.edu.al>
2021-02-26 21:04:34 +0100

    Final Push Small Bugs corrected related to Add Reviews without logging in and Back Button from book details when a user is not logged in. API -> BookData will be returned instead of raw Books as per 		professor's advice.

--- src/java/API/BooksFilterAPI.java
+++ src/java/API/BooksFilterAPI.java
@@ -7,6 +7,7 @@ package API;
 
 import DAO.BookDAO;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -19,19 +20,37 @@ import models.Book;
 @WebService(serviceName = "BooksFilterAPI")
 public class BooksFilterAPI {
 
+    BookDAO bookDAO = new BookDAO();
      @WebMethod(operationName = "filterBooks")
-    public List<Book> filterBooks(@WebParam(name="title")String title,
+    public List<BookData> filterBooks(@WebParam(name="title")String title,
             @WebParam(name="author") String author,
             @WebParam(name="genre")String genre,
             @WebParam(name="minAverageRating")int minAverageRating, 
             @WebParam(name="maxAverageRating")int maxAverageRating)
     {
-        BookDAO bookDAO = new BookDAO();
+        
         if(minAverageRating==0 && maxAverageRating==0 && !bookDAO.isValueSet(title) && !bookDAO.isValueSet(author)
                 && !bookDAO.isValueSet(genre))//nothing is selected
             return null;
         
         return bookDAO.filterBooks(title, author, maxAverageRating, 
-                maxAverageRating, genre);
+                maxAverageRating, genre)
+                .stream()
+                .map(this::toBookData)
+                .collect(Collectors.toList());
+    }
+    
+    /**Method to convert a book object to a BookData object.
+     *@param b - is the book that we will convert
+     *@return a BookData object that will contain only the neccessary information 
+     */
+    private BookData toBookData(Book b){
+        double rating = bookDAO.getAverageRating(b.getId());
+        BookData bookData = new BookData();
+        bookData.setId(b.getId());
+        bookData.setTitle(b.getTitle());
+        bookData.setAuthor(b.getAuthor());
+        bookData.setBookRating(rating);
+        return bookData;
     }
 }
